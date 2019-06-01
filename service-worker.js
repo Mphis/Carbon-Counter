d6dc7f51faac70981ba608434aa77557c09f9cb3
Pranavi Choudhary <ppch09@gmail.com>
2019-06-01 18:16:27 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -50,3 +50,20 @@ if (evt.request.mode !== 'navigate') {
                 });
           })
   );
+
+navigator.serviceWorker.ready.then(function(registration) {
+  if (!registration.pushManager) {
+    alert('No push notifications support.');
+    return false;
+  }
+  //To subscribe `push notification` from push manager
+  registration.pushManager.subscribe({
+  userVisibleOnly: true //Always show notification when received
+  })
+  .then(function (subscription) {
+  console.log('Subscribed.');
+  })
+  .catch(function (error) {
+  console.log('Subscription error: ', error);
+  });
+})
